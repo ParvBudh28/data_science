30cbe381011e21f89578fddff68eae30c66434f8
Parv Budhiraja <parvbud@gmail.com>
2021-02-09 02:02:51 +0530

    add Python Basics module 4,5

---
+++ Python 4 (OOPS)/modules_in_python/math/simple.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+def mul(a, b):
+    return a*b
+
+def div(a, b):
+    return a//b
+
+# will be printed even if someone imports this file
+# print("hello")
+
+# how to tackle this problem
+if __name__=='__main__':
+    print("hello")
+
+# if we use the above conditional check then we can specify what all to run when we run this file
+# as a script by itself
